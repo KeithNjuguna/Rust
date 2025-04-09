f498972c9a41f4f0e42b0566c8feecd6f521c9f9
Keith Njuguna <164479322+KeithNjuguna@users.noreply.github.com>
2025-04-10 00:10:45 +0300

    Programming_a_geuss.rs

--- Chapter-2/Programming-a-geussing-game.rs
+++ Chapter-2/Programming-a-geussing-game.rs
@@ -1,1 +1,17 @@
 
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
+//This just asks you to geuss a number you input it and then it outputs the number a simple geuss basically.
